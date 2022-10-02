7100f82cfc208904e852d34e9e2cfffa9ec70ddf
Amizero-Tresor <peacemizero@gmail.com>
2022-10-02 20:21:03 +0200

    auto-commit-5b486f42285a4b91afbfe9393f6b6820

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,23 @@ import './header.css'
 const Header = () => {
   return (
     <div>
-      
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">Smith</a>
+
+
+
+        <div className="nav__menu">
+            <ul className="nav__list grid">
+                <li className="nav__item">
+                    <a href="" className="nav__link">
+                        <i className="uil uil- nav__icon">
+                            
+                        </i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+      </nav>
     </div>
   )
 }
