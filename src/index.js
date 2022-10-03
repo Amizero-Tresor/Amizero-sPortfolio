6d2026adba8becd58cb97c72514b85b909406708
Amizero-Tresor <peacemizero@gmail.com>
2022-10-03 09:35:16 +0200

    auto-commit-96a2e76feba641f7af35f225ef4f3d0d

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+import * as ReactDOMClient from 'react-dom/client';
 
+
+const root = ReactDOMClient.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+   
+  </React.StrictMode>
 );
-root.render(<App />);
